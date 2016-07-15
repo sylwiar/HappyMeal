e20dc885e3469e5f41f4248cd72b7f5e588e8e56
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-15 22:34:03 +0200

    Fixed logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root "orders#index"
 
   get "/auth/:provider/callback" => "sessions#create"
-  get "/signout" => "sessions#destroy", :as => :signout
+  get "/logout" => "sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
