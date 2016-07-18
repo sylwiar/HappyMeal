d99bbab4a71903e062fb30315e588d7c95ac0309
Sylwia Robak <sylwiar@sylwiar.(none)>
2016-07-18 22:57:53 +0200

    Fixed log in

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email,user:follow"
-  provider :facebook, ENV['FB_KEY'], ENV['FB_SECRET']
+  provider :github, 
+   Rails.application.secrets.GITHUB_KEY,
+   Rails.application.secrets.GITHUB_SECRET, scope: "user:email,user:follow"
+  provider :facebook,
+   Rails.application.secrets.FB_APP_ID,
+   Rails.application.secrets.FB_SECRET
 end
